603fbbdeb59f4f461d1c72477be61bec7bfa1adb
WAYNE_SEA <zzwwayne39@outlook.com>
2023-01-16 15:56:42 -0800

    Initial commit

---
+++ src/components/index.js
@@ -0,0 +1,29 @@
+import Navbar from "./Navbar";
+import CTA from "./CTA";
+import Stats from "./Stats";
+import Footer from "./Footer";
+import Billing from "./Billing";
+import CardDeal from "./CardDeal";
+import GetStarted from "./GetStarted";
+import Business from "./Business";
+import Clients from "./Clients";
+import Hero from "./Hero";
+import Testimonials from "./Testimonials";
+import FeedbackCard from "./FeedbackCard";
+import Button from "./Button";
+
+export {
+    Billing,
+    Business,
+    Button,
+    CardDeal,
+    Clients,
+    CTA,
+    FeedbackCard,
+    Footer,
+    GetStarted,
+    Hero,
+    Navbar,
+    Stats,
+    Testimonials,
+};
